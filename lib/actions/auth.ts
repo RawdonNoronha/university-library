a2877a0366125116b952a07f854ee4e27f093d21
RawdonNoronha <rawdonnoronha11@gmail.com>
2025-10-11 12:56:19 +0530

    fixed typo

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -69,7 +69,7 @@ export const signUp = async (params: AuthCredentials) => {
             universityCard,
         });
         await worflowClient.trigger({
-            url: `${config.env.prodApiEndpoint}/api/workflows/onboarding`,
+            url: `${config.env.prodApiEndpoint}/api/auth/workflows/onboarding`,
             body:{
                 email,
                 fullName
